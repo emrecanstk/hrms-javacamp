371bd437dc1896e4c160cf3453e43b0b6ce1595f
Emre Can Satık <emrecanstk@gmail.com>
2022-06-18 18:52:29 +0300

    Add interface

---
+++ hrms/src/main/java/com/emrecanstk/hrms/dataAccess/abstracts/UserDao.java
@@ -0,0 +1,9 @@
+package com.emrecanstk.hrms.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emrecanstk.hrms.entities.concretes.User;
+
+public interface UserDao extends JpaRepository<User, Integer> {
+	
+}
