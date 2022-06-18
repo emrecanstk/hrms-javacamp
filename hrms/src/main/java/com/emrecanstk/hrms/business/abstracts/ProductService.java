f5aa7e7cdae66f0d5e36efdf707d38e9fc6b4ff8
Emre Can Satık <emrecanstk@gmail.com>
2022-06-18 18:43:45 +0300

    Revert "Add interface"
    This reverts commit 7c9896859a83488d0ed9e13420781748d7cabf3a.

--- hrms/src/main/java/com/emrecanstk/hrms/business/abstracts/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.emrecanstk.hrms.business.abstracts;
-
-import java.util.List;
-
-import com.emrecanstk.hrms.entities.concretes.User;
-
-public interface ProductService {
-	List<User> getAll();
-}
